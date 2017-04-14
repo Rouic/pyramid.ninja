bc26f33903bdb7c1ec2942d99ef8c1fd5e7eb442
Alex Cottenham <alex@rouic.com>
2017-04-15 00:37:17 +0100

    Added rudimentary client connect socket exchange logic + UI
    *Added material design UI complete with boilerplate and JS listing
    *Added socket.io and express logic to connect clients and hosts
    *Added mobile recognition
    *Added angular logic to handle pages and controllers
    *Added backgrounds

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 var Rouic = angular.module('Rouic', ['ui.router']);
-
+var socket = io();
+var currentGame = null;
 
 Rouic.run(['$window', '$rootScope', '$state', '$stateParams', function($window, $rootScope, $state, $stateParams){
 	$rootScope.$state = $state;
@@ -9,7 +10,6 @@ Rouic.run(['$window', '$rootScope', '$state', '$stateParams', function($window, 
     	$window.history.back();
 	};	
 	
-	var socket = io();
 	socket.on('connect', function(){
 	    $rootScope.socketID = socket.io.engine.id;
 	});		
@@ -18,39 +18,78 @@ Rouic.run(['$window', '$rootScope', '$state', '$stateParams', function($window, 
 
 Rouic.config(function($stateProvider, $urlRouterProvider) { 
     
+    if(/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream){
+	    $urlRouterProvider.otherwise('/join');
+    } else {
+	    $urlRouterProvider.otherwise('/start');
+    }
     
-    $urlRouterProvider.otherwise('/home'); 
+     
     
     $stateProvider   
 	.state('root', {
-	          abstract: true,
-	          views: {
-		       '@' : {
-			      controller: 'root',
- 			      templateUrl: 'app/layout.html',
-		       },
-/*
-		       'headerBar': {
-			       templateUrl: 'app/layout/navigation/header.bar.html',
-			       controller: 'headerBar'
-		        },	            
-		       'headerMenu': {
-			       templateUrl: 'app/layout/navigation/menu.html', 
-			       controller: 'menuController'
-		        }
-*/
-	           }
-	        })	                
-    .state('home', {
+// 	    abstract: true,
+	    views: {
+		    '@' : {
+ 			    templateUrl: 'app/layout.html',
+ 			    controller: 'root',
+		    },
+		    'header': {
+			    templateUrl: 'app/header.html',
+			    controller: 'header'
+		    }
+	    }
+	})	                
+    .state('start', {
  	    parent: 'root',
-        url: '/home',
+        url: '/start',
         views: {
-	       'view': {
- 		       templateUrl: 'app/home.html',
-		       controller: 'home'
+	        'view': {
+ 		        templateUrl: 'app/start.html',
+		        controller: 'start'
 		    }
         }        
-    });
+    })
+    .state('host', {
+ 	    parent: 'root',
+        url: '/host',
+        views: {
+	        'view': {
+ 		        templateUrl: 'app/host.html',
+		        controller: 'host'
+		    }
+        }        
+    })
+    .state('join', {
+ 	    parent: 'root',
+        url: '/join',
+        views: {
+	        'view': {
+ 		        templateUrl: 'app/join.html',
+		        controller: 'join'
+		    }
+        }        
+    })    
+    .state('game', {
+ 	    parent: 'root',
+        url: '/game/:gameID?',
+        views: {
+	        'view': {
+ 		        templateUrl: 'app/game.html',
+		        controller: 'game'
+		    }
+        }        
+    })       
+    .state('about', {
+ 	    parent: 'root',
+        url: '/about',
+        views: {
+	        'view': {
+ 		        templateUrl: 'app/about.html',
+		        controller: 'about'
+		    }
+        }        
+    });    
 
   
 });
@@ -58,13 +97,140 @@ Rouic.config(function($stateProvider, $urlRouterProvider) {
 
 
 Rouic.controller('root', function($state, $scope, $rootScope, $stateParams){
+	
+});
+
+Rouic.controller('header', function($state, $scope, $rootScope, $stateParams){
 
 });
 
-Rouic.controller('home', function($state, $scope, $rootScope, $stateParams){
+Rouic.controller('start', function($state, $scope, $rootScope, $stateParams){
+	$rootScope.pageClass = 'signup-page';
+	$.material.init();
+	if(currentGame){
+		socket.emit('leave', {room: currentGame});
+		currentGame = null;
+	}
 	
-	$scope.$watch('$rootScope.socketID', function() {
-	    $scope.socketID = $rootScope.socketID;
+});
+
+Rouic.controller('host', function($state, $scope, $rootScope, $stateParams, $interval){
+	$rootScope.pageClass = 'signup-page';
+	$.material.init();
+	$scope.domain = window.location.hostname;
+	
+	socket.emit('newRoom');
+	socket.on('newRoomSuccess', function(msg){
+		currentGame = msg.room;
+		$scope.roomCode = msg.room.toUpperCase();
+		$scope.$apply();
 	});
 	
+	socket.on('newGameClients', function(msg){
+		$scope.players = msg.data;
+		console.log(msg.data);
+		$scope.$apply();
+	});
+	
+});
+
+Rouic.controller('join', function($state, $scope, $rootScope, $stateParams){
+	$rootScope.pageClass = 'signup-page';
+	$.material.init();	
+	
+	$scope.join = {};
+	
+	$scope.joinGame = function(){
+		socket.emit('joinRoom', {room: $scope.join.roomcode.toLowerCase(), name: 'Boop'});
+		socket.on('joinRoomResponce', function(msg){
+			socket.off('joinRoomResponce');
+			if(msg.validity == true){
+				currentGame = $scope.join.roomcode;
+				$state.go('game', {gameID: $scope.join.roomcode})
+			} else {
+				$scope.joinError = msg.error;
+				console.log(msg.error);
+				$scope.$apply();
+			}
+		});		
+	};
+	
+});
+
+Rouic.controller('game', function($state, $scope, $rootScope, $stateParams){
+	$rootScope.pageClass = 'signup-page';
+	$.material.init();
+	
+	if(!$stateParams.gameID){
+		$state.go('start');
+	} else {
+		roomCode = $stateParams.gameID;
+		currentGame = $stateParams.gameID;
+		console.log("Loading Game: "+$stateParams.gameID);
+		
+		socket.emit('joinRoom', {room: $stateParams.gameID.toLowerCase(), name: 'Boop'});
+		socket.on('joinRoomResponce', function(msg){
+			socket.off('joinRoomResponce');
+			if(msg.validity == true){
+				currentGame = $stateParams.gameID;
+			} else {
+				$scope.joinError = msg.error;
+				console.log(msg.error);
+				$scope.$apply();
+			}
+		});			
+		
+		
+	}
+	
+	socket.on('hostLeft', function(){
+		$state.go('join');
+	})
+	
+	
+});
+
+Rouic.controller('about', function($state, $scope, $rootScope, $stateParams){
+	$rootScope.pageClass = 'about-us';
+    $.material.init();
+
+    window_width = $(window).width();
+
+    //  Activate the Tooltips
+    $('[data-toggle="tooltip"], [rel="tooltip"]').tooltip();
+
+    // Activate Datepicker
+    if($('.datepicker').length != 0){
+        $('.datepicker').datepicker({
+             weekStart:1
+        });
+    }
+
+    //    Activate bootstrap-select
+    $(".select").dropdown({ "dropdownClass": "dropdown-menu", "optionClass": "" });
+
+    // Activate Popovers
+    $('[data-toggle="popover"]').popover();
+
+    // Active Carousel
+	$('.carousel').carousel({
+      interval: 400000
+    });
+
+    //Activate tags
+    if($(".tagsinput").length != 0){
+        $(".tagsinput").tagsInput();
+    }
+
+    if($('.navbar-color-on-scroll').length != 0){
+        $(window).on('scroll', materialKit.checkScrollForTransparentNavbar)
+    }
+
+    if (window_width >= 768){
+        big_image = $('.page-header[data-parallax="active"]');
+        if(big_image.length != 0){
+            $(window).on('scroll', materialKitDemo.checkScrollForParallax);
+        }
+
+    }
 });
--- server.js
+++ server.js
@@ -73,10 +73,110 @@ httpServer.listen(webPort);
 app = httpServer;
 console.log("[BOOT] Web server listening on port: "+webPort);
 
-
 io = io(app);
-io.on('connection', function(socket){ //waiting for socket connection, on connection, get socket
-		console.log("[INFO] A user connected to socket.io!");
+io.on('connection', function(socket){
+
+	socket.on('leave', function(msg){
+						
+	   		if(socket.clientOf){
+		   		socket.leave(socket.clientOf);	
+	   			if(io.sockets.adapter.rooms[socket.clientOf]){
+					var clients = io.sockets.adapter.rooms[socket.clientOf].sockets;  
+					var gameClients = []; 
+					for (var clientId in clients ) {
+					     var clientSocket = io.sockets.connected[clientId];				
+						gameClients.push({name: clientSocket.name, type: clientSocket.type});
+					
+					}			
+					io.to(socket.clientOf).emit('newGameClients', {data: gameClients});
+					socket.clientOf = null;	
+				}		
+	   		} else if(socket.hostOf){
+		   		socket.leave(socket.hostOf);	
+	   			io.to(socket.hostOf).emit('hostLeft');
+	   			socket.hostOf = null;
+	   		} 
+	   		
+		
+	});
+
+
+   socket.on('disconnect', function() {
+   		if(socket.type == 'client'){
+	   		if(socket.clientOf){
+				var gameClients = [];
+				if(io.sockets.adapter.rooms[socket.clientOf]){
+					var clients = io.sockets.adapter.rooms[socket.clientOf].sockets;   
+					for (var clientId in clients ) {
+					     var clientSocket = io.sockets.connected[clientId];				
+						gameClients.push({name: clientSocket.name, type: clientSocket.type});
+					
+					}
+					console.log("[INFO] informing host about client changes to "+socket.clientOf);	
+					io.to(socket.clientOf).emit('newGameClients', {data: gameClients});	   
+				}		
+	   		}
+   		} else if(socket.type == 'host'){
+	   		if(socket.hostOf){
+	   			io.to(socket.hostOf).emit('hostLeft');
+	   		}
+	   	}
+   });
+
+
+	socket.on('newRoom', function(msg){
+		
+		var emptyString = "";
+		var alphabet = "abcdefghijklmnopqrstuvwxyz";
+		
+		while (emptyString.length < 4) {
+		  emptyString += alphabet[Math.floor(Math.random() * alphabet.length)];
+		} 		
+		
+		var roomPIN = emptyString;
+		socket.type = 'host';
+		socket.hostOf = roomPIN;
+		console.log("New Room Pin: "+roomPIN);
+		console.log("[INFO] Creating new host with room code: "+roomPIN);
+		socket.emit('newRoomSuccess', {room: roomPIN});
+		socket.join(roomPIN);
+	});
+	
+	socket.on('joinRoom', function(msg){
+		if(msg.room && msg.name){
+			
+			
+			console.log("[INFO] joining "+msg.name+" to "+msg.room);
+			
+			if(io.nsps["/"].adapter.rooms[msg.room]){
+				socket.type = 'client';
+				socket.clientOf = msg.room;
+				socket.name = msg.name;
+				socket.join(msg.room);
+				socket.emit('joinRoomResponce', {validity: true});
+				
+				var gameClients = [];
+				var clients = io.sockets.adapter.rooms[msg.room].sockets;   
+				var numClients = (typeof clients !== 'undefined') ? Object.keys(clients).length : 0;
+				for (var clientId in clients ) {
+				
+				     var clientSocket = io.sockets.connected[clientId];
+// 				     clientSocket.emit('new event', "Updates");
+				
+					gameClients.push({name: clientSocket.name, type: clientSocket.type});
+				
+				}			
+				console.log("[INFO] informing host about client changes to "+msg.room);
+				io.to(msg.room).emit('newGameClients', {data: gameClients});
+			} else {
+				socket.emit('joinRoomResponce', {validity: false, error: 'unknown game code'});
+			}
+		} else {
+			socket.emit('joinRoomResponce', {validity: false, error: 'missing details'});
+		}
+	});	
+
+
 		
 });
    
