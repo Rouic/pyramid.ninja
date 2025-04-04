6b593a6513fdd300a673f14d4ea5a3fdc3965a8f
Alex Cottenham <alex@rouic.com>
2025-04-04 13:47:29 +0100

    Removed legacy codebase

--- legacy/functions/index.js
+++
@@ -1,8 +0,0 @@
-const functions = require('firebase-functions');
-const express = require('express');
-const avatarsMiddleware = require('adorable-avatars');
-
-const app = express();
-app.use('/', avatarsMiddleware);
-
-exports.avatars = functions.https.onRequest(app);
